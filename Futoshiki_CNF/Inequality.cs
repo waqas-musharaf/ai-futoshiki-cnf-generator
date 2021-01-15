b19cec4c4535c028c870ad63e9805c53cb9d26fb
Waqas Musharaf <56737535+waqas-musharaf@users.noreply.github.com>
2021-01-15 21:20:49 +0000

    Added files via upload

---
+++ Futoshiki_CNF/Inequality.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Futoshiki_CNF
+{
+    public class Inequality
+    {
+        public int isLessRow;
+        public int isLessColumn;
+        public int isGreaterRow;
+        public int isGreaterColumn;    
+
+        public Inequality() { }
+
+        public Inequality(int isLessRow, int isLessColumn, int isGreaterRow, int isGreaterColumn)
+        {                    
+            this.isLessRow = isLessRow;
+            this.isLessColumn = isLessColumn;
+            this.isGreaterRow = isGreaterRow;
+            this.isGreaterColumn = isGreaterColumn;
+        }
+    }
+}
